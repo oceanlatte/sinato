3feda94e9f52b1859ecaa8be17f717ecbad7ab13
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-08-31 23:07:03 -0500

    get one post route works

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -31,8 +31,34 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_content', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
 router.post('/', (req, res) => {
-    // expects {title: 'Taskmaster goes public!', text_content: 'Crazy that no one expected it.', user_id: 1}
+    // expects {title: 'Taskmaster goes public!', post_content: 'Crazy that no one expected it.', user_id: 1}
     console.log(req.body),
     //console.log(req.session.user_id);
     Post.create({
