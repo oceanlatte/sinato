3a0c14f534c3a9acbb6cf7cc79c2ae36b648af55
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-09-01 22:51:24 -0500

    get and get:id routes written for homepage but I need to figure out how to test them

---
+++ controllers/home-routes.js
@@ -0,0 +1,104 @@
+const router = require("express").Router();
+const { json } = require("express/lib/response");
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+    console.log('===========Home Page============');
+    console.log(req.session);
+    Post.findAll({
+        attributes: [
+            "id",
+            "title",
+            "post_content",
+            "create_at",
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                    "created_at"
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then(dbPostData=> {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+
+        res.render('homepage', {
+           body
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get("/post/:id", (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            "id",
+            "title",
+            "text_content",
+            "created_at"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "post_id",
+                    "user_id",
+                    "created_at"
+                ],
+                include:{
+                    model: User,
+                    attributes:["username"]
+                }
+            },
+            {
+                model: User,
+                attributes:["username"]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData){
+            res.status(404).json({message: "No post found with this id"});
+            return;
+        }
+        const post = dbPostDATA.GET({ plain: true });
+        console.log(post);
+
+        res.render("single-post", {
+            post
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
