6e0027a0dc089595ed99ded1d0f674f9b9d05eef
ocean <osciris.veliz@gmail.com>
2022-09-02 22:56:16 -0500

    save user data to session in users/post and users/login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -43,7 +43,16 @@ router.post("/", (req, res) => {
     username: req.body.username,
     password: req.body.password,
   })
-    .then((dbUserData) => res.json(dbUserData))
+    .then((dbUserData) => {
+      // save user data to session
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -69,9 +78,14 @@ router.post("/login", (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: "You are now logged in!" });
+    // Verify user sessions
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
 
-    // Verify user sessions <=======================
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
   });
 });
 
