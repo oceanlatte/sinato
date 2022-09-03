459351de69e2361333076c0509770d043ebcdc6e
ocean <osciris.veliz@gmail.com>
2022-09-02 22:22:03 -0500

    add session variables to server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+const session = require("express-session");
 const express = require("express");
 
 const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const sess = {
+    secret: "Secrets",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
