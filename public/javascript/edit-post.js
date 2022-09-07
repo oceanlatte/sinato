330cf13393cfa11027af274bd19066ecceaa58ca
SRoss <srossouw@uwm.edu>
2022-09-07 08:00:33 -0500

    added emoji picker to add post and edit comments

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,31 +1,43 @@
+$(document).ready(function() {
+    $("#emojionearea1").emojioneArea({
+        pickerPosition: "bottom",
+        tonesStyle: "bullet"
+    });
+})
+
 async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value.trim();
     const artist = document.querySelector('input[name="artist"]').value.trim();
     const post_content = document.querySelector('input[name="post-content"]').value.trim();
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+    console.log(title);
+    console.log(artist);
+    console.log(post_content);
 
-    const response = await fetch(`/api/posts/${id}`, {
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+      
+      const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            title,
-            artist,
-            post_content
-
+          post_id: id,
+          artist,
+          title,
+          post_content
         }),
         headers: {
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
         }
-    });
-
-    if (response.ok) {
+      });
+      
+      if (response.ok) {
         document.location.replace('/dashboard/');
-    } else {
+      } else {
         alert(response.statusText);
-    }
+      }
+
 }
 
 document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
